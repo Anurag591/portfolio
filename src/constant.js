ebf4d288bc5b0f56560bdca10cfb8d40fc65b01a
Anurag Singh <anuragsngh462@gmail.com>
2021-07-11 10:53:44 +0530

    updated lock file

---
+++ src/constant.js
@@ -0,0 +1,115 @@
+export const production = false;
+export const name = 'Abhinav Anand';
+export const email ='abhinav1813@gmail.com';
+export const CURRENT_DESIGNATION = 'Software Engineer';
+export const PROFILE = ['Developer', 'Freelancer', 'Tester'];
+export const socialLinks = [
+    { title: 'Twitter', link: '', className: 'fab fa-twitter', color: '#5DA9DD' },
+    { title: 'Facebook', link: 'https://www.facebook.com/24abhinav', className: 'fab fa-facebook-f', color: '#4867AA' },
+    { title: 'Instagram', link: '', className: 'fab fa-instagram' , color: '#B91E6D'},
+    { title: 'Skype', link: 'https://join.skype.com/invite/opBO4rGZv3wG', className: 'fab fa-skype', color: '#0079C0' },
+    { title: 'LinkedIn', link: 'https://www.linkedin.com/in/24abhinav/', className: 'fab fa-linkedin-in', color: '#0A66C2' },
+];
+
+export const aboutMe = `
+    Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem.
+    Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.
+    Quia fugiat sit in iste officiis commodi quidem hic quas.
+`;
+
+export const programmingSkills = [
+    { skillName: 'html 5', rating: '90%' },
+    { skillName: 'css 3', rating: '85%' },
+    { skillName: 'javascript (ES8)', rating: '95%' },
+    { skillName: 'React.Js', rating: '85%' },
+    { skillName: 'redux', rating: '90%' },
+    { skillName: 'typescript', rating: '90%' },
+    { skillName: 'Angular 2', rating: '70%', sup: '+' },
+    { skillName: 'Node.Js', rating: '65%' },
+    { skillName: 'Express', rating: '65%' },
+    { skillName: 'SailsJs', rating: '70%' },
+];
+
+export const technicalSkills =[
+    { skillName: 'Agile Methodology', rating: '90%' },
+    { skillName: 'Jira', rating: '90%' },
+    { skillName: 'Git', rating: '95%' },
+    { skillName: 'Webpack', rating: '75%' },
+    { skillName: 'Vs Code', rating: '90%' },
+    { skillName: 'Sublime Text', rating: '90%' },
+];
+
+export const AboutInfo = [
+    {keyName: 'BirthDay', value: '31 July 1995'},
+    {keyName: 'Age', value: '25'},
+    {keyName: 'Website', value: 'www.anuragsingh.com'},
+    {keyName: 'Degree', value: 'B.Tech (CS)'},
+    {keyName: 'Phone', value: '+91 7001208860'},
+    {keyName: 'Email', value: 'anuragsngh462@gmail.com'},
+    {keyName: 'City', value: 'Jodhpur'},
+    {keyName: 'Freelance', value: 'Available'},
+];
+
+export const PROJECT_LIST = [
+    {
+        company_name: 'Caritor Solutions India Pvt Ltd',
+        projects: [
+            {
+                title: 'Religare Health Insurance',
+                description: `Religare Health Insurance Company Limited is an Indian health insurance firm.`,
+                duration: 'June 2019 – Jan 2021 (1 Years & 8 Months)',
+                designation: 'Software Engineer',
+                place: 'Bangalore',
+                responsibility: [
+                    'Development of business logic using ReactJs.',
+                    'State management using Redux.',
+                    'Creating template using HTML, CSS and Bootstrap.'
+                ]
+            },
+            {
+                title: 'Religare Health Insurance',
+                description: `Religare Health Insurance Company Limited is an Indian health insurance firm.`,
+                duration: 'June 2019 – Jan 2021 (1 Years & 8 Months)',
+                designation: 'Software Engineer',
+                place: 'Bangalore',
+                responsibility: [
+                    'Development of business logic using ReactJs.',
+                    'State management using Redux.',
+                    'Creating template using HTML, CSS and Bootstrap.'
+                ]
+            }
+        ]
+    },
+    {
+        company_name: 'Mantra Labs Pvt Ltd',
+        projects: [
+            {
+                title: 'Religare Health Insurance',
+                description: `Religare Health Insurance Company Limited is an Indian health insurance firm.`,
+                duration: 'June 2019 – Jan 2021 (1 Years & 8 Months)',
+                designation: 'Software Engineer',
+                place: 'Bangalore',
+                responsibility: [
+                    'Development of business logic using ReactJs.',
+                    'State management using Redux.',
+                    'Creating template using HTML, CSS and Bootstrap.'
+                ]
+            },
+            {
+                title: 'Religare Health Insurance',
+                description: `Religare Health Insurance Company Limited is an Indian health insurance firm.`,
+                duration: 'June 2019 – Jan 2021 (1 Years & 8 Months)',
+                designation: 'Software Engineer',
+                place: 'Bangalore',
+                responsibility: [
+                    'Development of business logic using ReactJs.',
+                    'State management using Redux.',
+                    'Creating template using HTML, CSS and Bootstrap.'
+                ]
+            }
+        ]
+    }
+];
+
+
+export const pdfUrl = 'https://propcatalyst-dev.s3.ap-south-1.amazonaws.com/propcatalyst-assets/Abhinav%20Anand%20CV.pdf';
